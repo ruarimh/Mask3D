578461d968171989d82754606f9149f32f219cc2
ruarimh <ram212@cam.ac.uk>
2023-05-18 20:56:46 +0100

    Add .las compatability

--- benchmark/evaluate_semantic_instance.py
+++ benchmark/evaluate_semantic_instance.py
@@ -400,6 +400,23 @@ def evaluate(preds: dict, gt_path: str, output_file: str, dataset: str = "scanne
         for i in range(len(VALID_CLASS_IDS)):
             LABEL_TO_ID[CLASS_LABELS[i]] = VALID_CLASS_IDS[i]
             ID_TO_LABEL[VALID_CLASS_IDS[i]] = CLASS_LABELS[i]
+            
+    if dataset == "las":
+        # global CLASS_LABELS
+        # global VALID_CLASS_IDS
+        # global ID_TO_LABEL
+        # global LABEL_TO_ID
+
+        opt['min_region_sizes'] = np.array([10])
+
+        CLASS_LABELS = ["Other", "Trees"]
+        VALID_CLASS_IDS = np.array([1])
+
+        ID_TO_LABEL = {}
+        LABEL_TO_ID = {}
+        for i in range(len(VALID_CLASS_IDS)):
+            LABEL_TO_ID[CLASS_LABELS[i]] = VALID_CLASS_IDS[i]
+            ID_TO_LABEL[VALID_CLASS_IDS[i]] = CLASS_LABELS[i]
 
     if dataset == "s3dis":
         # global CLASS_LABELS
