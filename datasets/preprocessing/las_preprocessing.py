5eebba88f606cef3cf571336d2a14009353ad044
ruarimh <ram212@cam.ac.uk>
2023-06-04 22:25:50 +0100

    fix numpy tuple | bug, for real this time

--- datasets/preprocessing/las_preprocessing.py
+++ datasets/preprocessing/las_preprocessing.py
@@ -84,8 +84,8 @@ class LASPreprocessing(BasePreprocessing):
         
         # remove points that are unannotated (0) or trees that were not labelled (3)
         points = np.delete(points, np.where(
-            (points["raw_classification"] == 0)) | 
-            (points["raw_classification"] == 3),
+            (points["raw_classification"] == 0) | 
+            (points["raw_classification"] == 3)),
             axis = 0)
 
         # following the stpls3d format
