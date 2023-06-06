efba7f024a843c298e4a59a71ceb7b6677491a7c
ruarimh <ram212@cam.ac.uk>
2023-06-06 17:58:00 +0100

    Revert "decreased len() > x to x = 1"
    This reverts commit 5cafef61339f274e0b4c9f637c9f94dbf4c24eb1.

--- datasets/preprocessing/las_preprocessing.py
+++ datasets/preprocessing/las_preprocessing.py
@@ -177,7 +177,7 @@ class LASPreprocessing(BasePreprocessing):
             filebase["filepath_crop"] = []
                 
             for block_id, block in enumerate(blocks):
-                if len(block) > 1:
+                if len(block) > 10:
                     if mode == "validation":
                         new_instance_ids = np.unique(block[:, -1], return_inverse=True)[1]
 
@@ -200,7 +200,7 @@ class LASPreprocessing(BasePreprocessing):
                     np.save(processed_filepath, block.astype(np.float32))
                     filebase["filepath_crop"].append(str(processed_filepath))
                 else:
-                    print("block was smaller than 1 points")
+                    print("block was smaller than 10 points")
                     assert False
 
         filebase["color_mean"] = [
