dc70371fe8d51d95a81719cc5dc0a3799deb177e
ruarimh <ram212@cam.ac.uk>
2023-06-14 16:57:19 +0100

    changed random size to a tuple

--- datasets/preprocessing/las_preprocessing.py
+++ datasets/preprocessing/las_preprocessing.py
@@ -100,9 +100,9 @@ class LASPreprocessing(BasePreprocessing):
         
         if not self.use_rgb:
             # replace all colour values with 255.0
-            points["red"] = np.random.random((points["red"].shape))
-            points["green"] = np.random.random((points["green"].shape))
-            points["blue"] = np.random.random((points["blue"].shape))
+            points["red"] = np.random.random((points["red"].shape,))
+            points["green"] = np.random.random((points["green"].shape,))
+            points["blue"] = np.random.random((points["blue"].shape,))
             
             points["red"] += 1
             points["green"] += 1
