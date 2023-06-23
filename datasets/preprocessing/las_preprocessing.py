fcf38a2e7297a03c847015d446aab689de0f0a50
ruarimh <ram212@cam.ac.uk>
2023-06-23 22:16:49 +0100

    added comments and docstrings

--- datasets/preprocessing/las_preprocessing.py
+++ datasets/preprocessing/las_preprocessing.py
@@ -23,14 +23,15 @@ class LASPreprocessing(BasePreprocessing):
         
         """
         Args:
-            data_dir (str): Directory containing the train, validationm and test directories which contain .las files.
+            data_dir (str): Directory containing the train, validation and test directories which contain .las files.
             save_dir (str): Directory to save the processed data.
             modes (tuple): Modes to process ("train", "validation", "test").
             n_jobs (int): Number of parallel jobs for processing.
             sample_proportion (float): Proportion of points to sample.
             use_rgb (bool): Whether to use RGB values.
-            subplot_size (float): Size of subplots for validation mode.
-
+            subplot_size (float): Size of subplots used during training / validation.
+                                  If subplot size > the size of the whole plot,
+                                  then the whole plot will be used.
         """
         
         super().__init__(data_dir, save_dir, modes, n_jobs, sample_proportion, 
@@ -40,7 +41,6 @@ class LASPreprocessing(BasePreprocessing):
         # the "Other" class contains the ground and low vegetation
         VALID_CLASS_IDS = np.array([1])  
 
-        # consider adding ground class
         self.class_map = {
             "Other": 0,
             "Trees": 1,
@@ -59,6 +59,13 @@ class LASPreprocessing(BasePreprocessing):
             self.files[mode] = natsorted(filepaths)
 
     def create_label_database(self):
+        """
+        Create the label database and save it as a YAML file.
+
+        Returns:
+            label_database (dict): The label database.
+
+        """
         label_database = dict()
         for class_name, class_id in self.class_map.items():
             label_database[class_id] = {
@@ -71,13 +78,35 @@ class LASPreprocessing(BasePreprocessing):
         return label_database
 
     def process_file(self, filepath, mode):
-        """process_file.
-        Please note, that for obtaining segmentation labels ply files were used.
+        """
+        Process a .las file and save the processed data.
+        
+        For the FORinstance NIBIO2 dataset, las.points.array has the following structure:
+            X: int
+            Y: int
+            Z: int
+            intensity: int
+            bit_fields: int
+            raw_classification: int - see Classification in FORinstance_dataset/FORinstance_readMe.txt
+            scan_angle_rank: int
+            user_data: int - empty and unused
+            point_source_id: int
+            gps_time: int
+            red: int
+            green: int
+            blue: int
+            zASL: float
+            treeID: int - unique identifier for each annotated tree
+            treeSP: int - see treeSP in FORinstance_dataset/readMe.txt
+            
+
         Args:
-            filepath: path to the main ply file
-            mode: train, test or validation
+            filepath (str): Path to the main .las file.
+            mode (str): Mode (train, test, or validation).
+
         Returns:
-            filebase: info about file
+            filebase (dict): Information about the processed file.
+
         """
         filebase = {
             "filepath": filepath,
@@ -88,6 +117,7 @@ class LASPreprocessing(BasePreprocessing):
 
         with laspy.open(filepath) as fh:
             las = fh.read()
+
         
         points = las.points.array
         
@@ -174,12 +204,14 @@ class LASPreprocessing(BasePreprocessing):
         file_len = len(points)
         filebase["file_len"] = file_len
 
+        # save numpy array
         processed_filepath = self.save_dir / mode / f"{filebase['scene'].replace('.las', '')}.npy"
         if not processed_filepath.parent.exists():
             processed_filepath.parent.mkdir(parents=True, exist_ok=True)
         np.save(processed_filepath, points.astype(np.float32))
         filebase["filepath"] = str(processed_filepath)
 
+        # generate subplots for validation and testing point clouds
         if mode in ["validation", "test"]:
             
             filebase["instance_gt_filepath"] = []
@@ -201,17 +233,20 @@ class LASPreprocessing(BasePreprocessing):
     
                             gt_data = (block[:, -2]) * 1000 + new_instance_ids
     
+                            # save ground-truth txt file
                             processed_gt_filepath = self.save_dir / "instance_gt" / mode / f"{filebase['scene'].replace('.las', '')}_{block_id}.txt"
                             if not processed_gt_filepath.parent.exists():
                                 processed_gt_filepath.parent.mkdir(parents=True, exist_ok=True)
                             np.savetxt(processed_gt_filepath, gt_data.astype(np.int32), fmt="%d")
                             filebase["instance_gt_filepath"].append(str(processed_gt_filepath))
     
+                        # save numpy array
                         processed_filepath = self.save_dir / mode / f"{filebase['scene'].replace('.las', '')}_{block_id}.npy"
                         if not processed_filepath.parent.exists():
                             processed_filepath.parent.mkdir(parents=True, exist_ok=True)
                         np.save(processed_filepath, block.astype(np.float32))
                         filebase["filepath_crop"].append(str(processed_filepath))
+                        
                     else:
                         print("block was smaller than 10 points")
                         assert False
@@ -231,6 +266,13 @@ class LASPreprocessing(BasePreprocessing):
     def compute_color_mean_std(
             self, train_database_path: str = "./data/processed/las/train_database.yaml"
     ):
+        """
+        Compute the mean and standard deviation of color values from the training database.
+    
+        Args:
+            train_database_path (str): Path to the training database YAML file.
+    
+        """
         train_database = self._load_yaml(train_database_path)
         color_mean, color_std = [], []
         for sample in train_database:
@@ -246,6 +288,18 @@ class LASPreprocessing(BasePreprocessing):
         self._save_yaml(self.save_dir / "color_mean_std.yaml", feats_mean_std)
 
     def splitPointCloud(self, cloud, size=50.0, stride=50):
+        """
+        Split a point cloud into blocks of a specified size.
+    
+        Args:
+            cloud (numpy.ndarray): Input point cloud.
+            size (float): Size of the blocks.
+            stride (int): Stride value for splitting the cloud.
+    
+        Returns:
+            blocks (list): List of blocks containing points.
+    
+        """
         limitMax = np.amax(cloud[:, 0:3], axis=0)
         width = int(np.ceil((limitMax[0] - size) / stride)) + 1
         depth = int(np.ceil((limitMax[1] - size) / stride)) + 1
